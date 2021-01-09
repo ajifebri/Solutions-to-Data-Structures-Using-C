176b4712035cf575744046f3d083227d6d7bc144
Aji <febriatmoko@gmail.com>
2021-01-09 20:53:47 +0700

    Add radix sort in snippets

---
+++ snippets/radix.c
@@ -0,0 +1,68 @@
+#define NUMELTS 1000
+
+void radixsort(int x[], int n) {
+    int front[10], rear[10];
+    struct {
+        int info;
+        int next;
+    } node[NUMELTS];
+
+    int exp, first, i, j, k, p, q, y;
+
+    /* Initialize linked list */
+    for (i=0; i<n-1; i++) {
+        node[i].info = x[i];
+        node[i].next = i+1;
+    }
+    node[n-1].info = x[n-1];
+    node[n-1].next = -1;
+    first = 0; /* first is the head of the linked list */
+
+    for (k=1; k < 5; k++) {
+        /* Assume we have four-digit numbers */
+        for (i=0; i<10; i++) {
+            /* Initialize queues */
+            rear[i] = -1;
+            front[i] = -1;
+        }
+        /* Process each element on the list */
+        while (first != -1) {
+            p = first;
+            first = node[first].next;
+            y = node[p].info;
+            /* Extract the kth digit */
+            exp = power(10,k-1); /* raise 10 to (k-1)th power */
+            j = (y/exp)%10;
+            /* Insert y into queue[j] */
+            q = rear[j];
+            if (q == -1) 
+                front[j] = p;
+            else
+                node[q].next = p;
+            rear[j] = p;
+        }
+        /* At this point each record is in its proper queue */
+        /* based on digit k. We now form a single list from */
+        /* all the queue elements. Find the first element.  */
+        for (j=0; j<10 && front[j]==-1; j++)
+            ;
+        first = front[j];
+        /* Link up remaining queues */
+        while (j <= 9) {  /* Check if finished */
+            /* Find the next element */
+            for (i=j+1; i<10 && front[i]==-1; i++)
+                ;
+            if (i <= 9) {
+                p = i;
+                node[rear[j]].next = front[i];
+            }
+            j = i;
+        }
+        node[rear[p]].next = -1;
+    }
+    /* Copy back to original array */
+    for (i=0; i<n; i++) {
+        x[i] = node[first].info;
+        first = node[first].next;
+    }
+}
